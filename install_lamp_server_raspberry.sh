dfc33e5a6e57bb17ace82f14307528f40a0a717d
brunoSchuavab <schuavab@gmail.com>
2018-03-25 13:29:39 -0300

    add install lamp serve

---
+++ install_lamp_server_raspberry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install apache2 mysql-server php5 php5-mysql libapache2-mod-php5 -y
+sudo apt-get install phpmyadmin -y
+sudo apt-get install php5-curl php5-gd php5-mcrypt php5-xmlrpc -y
